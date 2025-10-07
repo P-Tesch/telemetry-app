1ca314826eec79df8609c14183cc906d1ff1cdcb
P-Tesch <pedrohenriquetesch@gmail.com>
2025-10-07 16:27:17 -0300

    Java setup (missing Docker)

---
+++ denormalization/src/main/java/com/tesch/denormalization/DenormalizationApplication.java
@@ -0,0 +1,13 @@
+package com.tesch.denormalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenormalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenormalizationApplication.class, args);
+	}
+
+}
---
+++ denormalization/src/test/java/com/tesch/denormalization/DenormalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesch.denormalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenormalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
